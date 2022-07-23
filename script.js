a17b61c81ba81e969230a484b8874db34095edf6
danny1417 <mr.mejia1417@gmail.com>
2022-07-23 00:12:03 -0400

    added map

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+const map = L.map('map', {
+    center: [48.868672, 2.342130],
+    zoom: 15,
+});
+
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '© OpenStreetMap'
 }).addTo(map);
 
-var marker = L.marker([51.5, -0.09]).addTo(map);
+
+const marker = L.marker([48.87007, 2.346453])
+marker.addTo(map).bindPopup('<p1><b>The Hoxton, Paris</b></p1>').openPopup()
